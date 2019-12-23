a4a871447d97995f445fee11a01bcdc32163c0e2
rohit645 <kumarnath.rohit@gmail.com>
2019-12-23 11:59:38 +0530

    used MongoDB to store data

--- index.js
+++ index.js
@@ -1,87 +1,55 @@
 // const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-var port = process.env.PORT || 3001
-// convert json obejct of req.body into javascript object and attaches it again to req.body 
-// before its route handle app.post is called 
+const port = process.env.PORT
+const Note = require('./models/note')
+
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
 
-var notes =  [
-      {
-        "id": 1,
-        "content": "HTML is easy",
-        "date": "2019-05-30T17:30:31.098Z",
-        "important": false
-      },
-      {
-        "id": 2,
-        "content": "Browser can execute only Javascript",
-        "date": "2019-05-30T18:39:34.091Z",
-        "important": true
-      },
-      {
-        "id": 3,
-        "content": "GET and POST are the most important methods of HTTP protocol",
-        "date": "2019-05-30T19:20:14.298Z",
-        "important": false
-      }
-    ]
-
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0;
-    return maxId + 1
-}
-
-app.get('/', (req, res) => {
-    console.log('Hey it s a home page!')
-    res.send('<h1>Hello world!!</h1>')
-})
-
 app.get('/api/notes', (req, res) => {
-    console.log('Hey i got a get request')
-    res.json(notes)
+    Note
+    .find({})
+    .then(notes => {
+        res.json(notes.map(note => note.toJSON()))
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-    // console.log(note);
-    if(note)
-        res.json(note)
-    else 
-        res.status(404).end()
+    Note.findById(req.params.id)
+    .then(note => {
+        res.json(note.toJSON())
+    })
 }) 
 
-app.post('/api//notes', (req, res) => {
-    console.log('Hey i got a post request')
+app.post('/api/notes', (req, res) => {
     const body = req.body
     if (!body.content) {
         return res.status(400).json({
             error: 'content missing'
-        })
-        
+        })   
     }
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: body.important || false,
-        date: new Date().toISOString(),
-        id: generateId()
-    }
-    console.log(note);
-    notes.concat(note)
-    res.json(note)
+        important: body.important || Math.random() > 0.5,
+        date: new Date().toISOString()
+    })
+    note.save()
+    .then(savedItem => {
+        console.log('note saved successfully')
+        res.json(savedItem.toJSON())
+    })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    const id = Number(req.params.id)
-    // const dummy_notes = notes
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
-})
+// app.delete('/api/notes/:id', (req, res) => {
+//     const id = Number(req.params.id)
+//     notes = notes.filter(note => note.id !== id)
+//     res.status(204).end()
+// })
 
 app.listen(port, () => {
     console.log(`server listening at port ${port}`)
---
+++ models/note.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, {useNewUrlParser: true})
+    .then(response => {
+        console.log('connected to database')
+    })
+    .catch(error => {
+        console.log('error connecting to database, ', error.message)
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
