dd35139df145bdcdd6f1073289a7c33f2d151686
rohit645 <kumarnath.rohit@gmail.com>
2019-12-24 01:29:02 +0530

    Implemented find update post functionalited of frontend

--- index.js
+++ index.js
@@ -19,11 +19,15 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-app.get('/api/notes/:id', (req, res) => {
+app.get('/api/notes/:id', (req, res, next) => {
     Note.findById(req.params.id)
     .then(note => {
-        res.json(note.toJSON())
+        if(note)
+            res.json(note.toJSON())
+        else 
+            res.status(404).end()
     })
+    .catch(error => next(error))
 }) 
 
 app.post('/api/notes', (req, res) => {
@@ -45,12 +49,45 @@ app.post('/api/notes', (req, res) => {
     })
 })
 
-// app.delete('/api/notes/:id', (req, res) => {
-//     const id = Number(req.params.id)
-//     notes = notes.filter(note => note.id !== id)
-//     res.status(204).end()
-// })
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+        res.status(204).send('Deleted successfully')
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+    const body = req.body
+    
+    const note = {
+        content: body.content,
+        important: !body.important
+    }
+    Note.findByIdAndUpdate(req.params.id, note, {new: true})
+        .then(updatedNote => {
+            res.json(updatedNote.toJSON())
+        })
+        .catch(error => next(error))
+})
 
 app.listen(port, () => {
     console.log(`server listening at port ${port}`)
 })
+
+const unknownEndPoint = (req, res) => {
+    res.status(404).send({error: "unknown endpoint No route matched the request"})
+
+}
+app.use(unknownEndPoint)
+
+const errorhandler = (error, req, res, next)  => {
+    console.error(error.message)
+    
+    if(error.name === 'CastError' && error.kind === 'ObjectId') {
+        return res.status(400).send({error: 'malformatted id '})
+    }
+
+    next(error)
+}
+app.use(errorhandler)
--- models/note.js
+++ models/note.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
+mongoose.set('useFindAndModify', false)
 
 mongoose.connect(url, {useNewUrlParser: true})
     .then(response => {
